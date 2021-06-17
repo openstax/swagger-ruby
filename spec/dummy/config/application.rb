110725a33f56151348d4087cd013b04840f8bbaf
JP Slavinsky <jpslav@gmail.com>
2021-06-17 14:51:25 -0600

    test rails 5 and 6

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -19,7 +19,7 @@ require "openstax_swagger"
 module Dummy
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults Rails::VERSION::STRING.to_f
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
@@ -27,4 +27,3 @@ module Dummy
     # the framework and any gems in your application.
   end
 end
-
